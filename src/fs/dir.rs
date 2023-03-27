bf863c130359fd1a68d68cda0d0a8158404a5317
wangdong.teshin <wangdong.teshin@bytedance.com>
2023-03-27 17:00:41 +0800

    增加目录功能

--- src/fs/dir.rs
+++ src/fs/dir.rs
@@ -1,3 +1,4 @@
+use std::io::ErrorKind;
 use std::path::Path;
 
 pub enum FileType{
@@ -7,7 +8,18 @@ pub enum FileType{
 }
 
 pub async fn exist<P:AsRef<Path>>(path:P,ty:FileType)->std::io::Result<bool>{
-    let meta = tokio::fs::metadata(path).await?;
+    let result = tokio::fs::metadata(path).await;
+    let meta = match result{
+        Ok(o) => o,
+        Err(err) => {
+            return match err.kind() {
+                ErrorKind::NotFound => Ok(false),
+                _ => {
+                    Err(err)
+                }
+            }
+        }
+    };
     Ok(match ty {
         FileType::FILE => meta.is_file(),
         FileType::DIR => meta.is_dir(),
